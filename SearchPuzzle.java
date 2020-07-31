870bafdaf52fc12213156c94d6fd7e7da40f6258
alized559 <alized559@gmail.com>
2020-07-31 15:17:27 +0300

    fixes #1, fixes #3

--- SearchPuzzle.java
+++ SearchPuzzle.java
@@ -5,18 +5,42 @@ import java.io.FileNotFoundException;
 public class SearchPuzzle {
   public static void main(String[] args) throws FileNotFoundException {
     Scanner input = new Scanner(System.in);
-    System.out.print("Enter the word: ");
-    String word = input.next();
+    System.out.print("Please enter a word to search: ");
     char[][] grid = createPuzzle("grid.txt");
     int n = grid.length, m = grid[0].length;
-    int[] result = search(grid, n, m, word);
-    System.out.println("Puzzle : ");
-    printGrid(grid);
-    if (result[0] == -1) {
-      System.out.println(word + " not found.");
-    } else {
-      System.out.println(word + " found: row " + (result[0] + 1) + ", column " +
-                        (result[1] + 1) + ", going " + (result[2] == 0 ? "down" : "right"));
+    boolean running = true;
+    while (running) {
+      String word = input.next();
+      System.out.println("Puzzle : ");
+      int[] result = search(grid, n, m, word);
+      printGrid(grid);
+      if (result[0] == -1) {
+        System.out.println(word + " not found.");
+      } else {
+        if (result[2] == 1) {
+          System.out.println(word + " found: row " + (result[0] + 1) + ", column "
+                             + (result[1] + 1) + ", " + "going right");
+        }
+        if (result[2] == 0) {
+          System.out.println(word + " found: row " + (result[0] + 1) + ", column "
+                             + (result[1] + 1) + ", " + "going down");
+        }
+        if (result[2] == -2) {
+          System.out.println(word + " found: row " + (result[0] + 1) + ", column "
+                             + (result[1] + 1) + ", " + "going left");
+        }
+        if (result[2] == 2) {
+          System.out.println(word + " found: row " + (result[0] + 1) + ", column "
+                             + (result[1] + 1) + ", " + "going up");
+        }
+        System.out.println("\nDo you want to continue? (enter # to stop): ");
+        String c = input.next();
+        if (c.equalsIgnoreCase("#")) {
+          running = false;
+        } else {
+          System.out.print("Please enter a word to search: ");
+        }
+      }
     }
   }
 
@@ -28,13 +52,12 @@ public class SearchPuzzle {
   public static char[][] createPuzzle(String file)
              throws FileNotFoundException {
     Scanner readFile = new Scanner(new File("grid.txt"));
-    int n = readFile.nextInt();
-    int m = readFile.nextInt();
-    String str = readFile.next();
+    int n = readFile.nextInt(), m = readFile.nextInt();
+    String readLine = readFile.next();
     char[][] grid = new char[n][m];
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < m; j++) {
-        grid[i][j] = str.charAt(j + i * m);
+        grid[i][j] = readLine.charAt(j + i * m);
       }
     }
     return grid;
@@ -79,6 +102,29 @@ public class SearchPuzzle {
     return true;
   }
 
+	/*
+   * Search for a word in the left direction
+   * @param grid  A 2D array representing grid
+   * @param i     An integer for row
+   * @param j     An integer for column
+   * @param n     An integer representing the number of rows
+   * @param word  A string for the word to search for
+   * @return true whether word is found in the grid otherwise false
+  */
+  public static boolean searchLeft(char[][] grid, int i, int j, int m, String word) {
+  	// when going left from position (i, j), you can only read j characters
+    if (j < word.length()) {
+      return false;
+    }
+    int index = 0;
+    for (int k = j; k >= 0 && index < word.length(); k--) {
+      if (grid[i][k] != word.charAt(index))
+        return false;
+      index++;
+    }
+    return true;
+  }
+
   /*
    * Search for a word in the downward direction
    * @param grid  A 2D array representing grid
@@ -103,6 +149,30 @@ public class SearchPuzzle {
     return true;
   }
 
+	/*
+   * Search for a word in the upward direction
+   * @param grid  A 2D array representing grid
+   * @param i     An integer for row
+   * @param j     An integer for column
+   * @param n     An integer representing the number of rows
+   * @param word  A string for the word to search for
+   * @return true whether word is found in the grid otherwise false
+  */
+  public static boolean searchUp(char[][] grid, int i, int j, int n,
+                                 String word) {
+    // when going up from position (i, j), you can only read i characters
+    if (i < word.length()) {
+      return false;
+    }
+    int index = 0;
+    for (int k = i; k >= 0 && index < word.length(); k--) {
+      if (grid[k][j] != word.charAt(index))
+        return false;
+      index++;
+    }
+    return true;
+  }
+
   /*
    * Search for a word in the grid
    * @param grid  A 2D array representing grid
@@ -114,22 +184,34 @@ public class SearchPuzzle {
   public static int[] search(char[][] grid, int n, int m, String word) {
     int[] result = new int[3]; // save here row, col, direction
     result[0] = -1; // used to indicate word not found
-    for(int i = 0; i < n; i++) {
-      for(int j = 0; j < m; j++) {
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
         if (grid[i][j] != word.charAt(0))
           continue;
-        if(searchRight(grid, i, j, m, word)) {
+        if (searchRight(grid, i, j, m, word)) {
             result[0] = i;
             result[1] = j;
             result[2] = 1;
             return result;
         }
-        if(searchDown(grid, i, j, n, word)) {
+        if (searchDown(grid, i, j, n, word)) {
             result[0] = i;
             result[1] = j;
             result[2] = 0;
             return result;
         }
+        if (searchUp(grid, i, j, n, word)) {
+					result[0] = i;
+					result[1] = j;
+					result[2] = 2;
+					return result;
+				}
+				if (searchLeft(grid, i, j, m, word)) {
+					result[0] = i;
+					result[1] = j;
+					result[2] = -2;
+					return result;
+				}
       }
     }
     return result;
